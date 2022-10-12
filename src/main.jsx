739777ad35615f7911ee8f40006a080bc8f45ecc
Ifeanyi Okwuobi <fredrickokwuobi@gmail.com>
2022-10-12 18:01:55 +0100

    feat: add match-sorter, localforage packages and outlet component from react router

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import ErrorPage from "./error-page";
 import "./index.css";
+import Contact from "./routes/contact";
+import Root from "./routes/root";
 
-const router = createBrowserRouter({
-  path: "/",
-  element: <div>Hello World</div>,
-});
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
